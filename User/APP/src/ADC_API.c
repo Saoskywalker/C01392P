fd1b91682befa0fea1596fe77bf9b2145d015a20
Aysi-E <773917760@qq.com>
2022-06-02 09:31:03 +0800

    添加备用二分法查表函数

--- User/APP/src/ADC_API.c
+++ User/APP/src/ADC_API.c
@@ -18,6 +18,92 @@ static UI08 F_C(UI08 cfdata)
 /******************************************
  * 二分法查表
  * ***************************************/
+/* static UI16 ADC_lookup(UI16 value, const UI16 *ptr, UI16 tab_leng)
+{
+    UI16 low, high, mid;
+
+    if (tab_leng <= 1)
+        return 0;
+
+    low = 1;
+    high = tab_leng;
+    if (value <= *ptr)
+    {
+        return 0;
+    }
+    if (value >= *(ptr + tab_leng - 1))
+    {
+        return tab_leng - 1;
+    }
+
+    while (1)
+    {
+        tab_leng = high - low + 1;
+        if (tab_leng & 0X01)
+        {
+            mid = (tab_leng >> 1) + low;
+            if (value == *(ptr + mid - 1)) //结束条件
+            {
+                return mid - 1;
+            }
+            else if (mid + 1 == high && mid - 1 == low)
+            {
+                if (value > *(ptr + mid - 1))
+                {
+                    return mid;
+                }
+                else
+                {
+                    return mid - 2;
+                }
+            }
+            else if (value > *(ptr + mid - 1))
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        else
+        {
+            mid = (tab_leng >> 1) + low - 1;
+
+            if (value >= *(ptr + mid - 1) && value <= *(ptr + mid)) //结束条件
+            {
+                if (value == *(ptr + mid - 1))
+                {
+                    return mid - 1;
+                }
+                else if (value == *(ptr + mid))
+                {
+                    return mid;
+                }
+                else
+                {
+                    if (value - *(ptr + mid - 1) >= *(ptr + mid) - value)
+                    {
+                        return mid;
+                    }
+                    else
+                    {
+                        return mid - 1;
+                    }
+                }
+            }
+            else if (value > *(ptr + mid))
+            {
+                low = mid + 2;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+} */
+
 static UI16 ADC_lookup(UI16 value, const UI16 *ptr, UI16 tab_leng)
 {
     UI16 low, high, mid, x1, y1;
